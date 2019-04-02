44d7db98c3a26d5dddd839a65567728433203dcd
lucaslenard <49175413+lucaslenard@users.noreply.github.com>
2019-04-01 20:57:25 -0500

    Update asset tag.sh

--- asset tag.sh
+++ asset tag.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-asset=$(curl -sku jamfadmin:jamf1234 -H "accept: text/xml" https://127.0.0.1:8443/JSSResource/computers/id/3000 | xmllint --xpath '/computer/general/asset_tag/text()' -)
+#Set Variables
+username=""
+password=""
+url=""
+
+
+#Gather device serial number
+serial=$(system_profiler SPHardwareDataType | awk '/Serial Number/{print $4}') 
+
+#Gather current asset tab using API
+asset=$(curl -sku $username:$password -H "accept: text/xml" https://$url/JSSResource/computers/serialnumber/$serial | xmllint --xpath '/computer/general/asset_tag/text()' -)
+
+#Prompt user to fill in Asset Tag if current value is empty
 
 if [[ $asset = "" ]]; then
+
+#Prompt user using osascript	
 	userValue=$(osascript << EOF
 		text returned of (display dialog "Hello, please enter your asset tag" default answer "AT0000" buttons {"OK"} default button 1)
 		EOF)
+		
+#Update Jamf Pro by sending Asset tag via inventory update
 		sudo jamf recon -assetTag $userValue
 	else 
-	echo "You're good!"
+	echo "Asset tag already present"
 fi
 
 
