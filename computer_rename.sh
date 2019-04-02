3d8882491e0731c622ec73ddccbf1ad0dee0c844
lucaslenard <49175413+lucaslenard@users.noreply.github.com>
2019-04-01 20:47:23 -0500

    Update computer_rename.sh

--- computer_rename.sh
+++ computer_rename.sh
@@ -3,7 +3,7 @@
 #gets current logged in user
 computerName=$(ls -l /dev/console | awk '{ print $3 }')
 
-#set all the name in all the places
+#set name in ComputerName, LocalHostName and HostName locations
 scutil --set ComputerName "$computerName"
 scutil --set LocalHostName "$computerName"
 scutil --set HostName "$computerName"
